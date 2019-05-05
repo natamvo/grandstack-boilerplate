1d2171bc7acb8ec91cccaefca13a96a65956856d
Natam Oliveira <natam.oliveira@ckl.io>
2019-05-05 00:40:58 -0300

    let user resolver as public

--- api/src/schemas/User/UseCases/Users.js
+++ api/src/schemas/User/UseCases/Users.js
@@ -3,9 +3,9 @@ import { ApolloError } from "apollo-server"
 
 export default async (obj, params, ctx, resolveInfo) => {
   /* Only the user or those who are admin can delete */
-  if (ctx.user.role.includes('ADMIN')) {
+  // if (ctx.user.role.includes('ADMIN')) {
     return neo4jgraphql(obj, params, ctx, resolveInfo, true)
-  } else {
-    throw new ApolloError('not_authorized', 405, ['You are not allowed to do that']);
-  }
+  // } else {
+  //   throw new ApolloError('not_authorized', 405, ['You are not allowed to do that']);
+  // }
 }
