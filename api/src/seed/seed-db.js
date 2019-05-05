40d6653888b111ccfef30664627f67daf7b46ce1
Natam Oliveira <natam.oliveira@ckl.io>
2019-05-05 16:15:47 -0300

    clean and update seed-db

--- api/src/seed/seed-db.js
+++ api/src/seed/seed-db.js
@@ -2,11 +2,11 @@ import ApolloClient from "apollo-client";
 import gql from "graphql-tag";
 import seedmutations from "./seed-mutations";
 import fetch from "node-fetch";
-import { HttpLink } from "apollo-link-http";
+import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: process.env.API_URL, fetch }),
+  link: new createHttpLink({ uri: `http://${process.env.VIRTUAL_HOST}`, fetch }),
   cache: new InMemoryCache()
 });
 
@@ -15,4 +15,4 @@ client
     mutation: gql(seedmutations)
   })
   .then(data => console.log(data))
-  .catch(error => console.error(error));
+  .catch(error => console.error(error.message));
